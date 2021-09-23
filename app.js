502971a60cc7518c901e7f8a99bcbd11a23391e8
Manish Jha <manishjhazyx@gmail.com>
2021-09-23 20:17:27 +0530

    initial commit

---
+++ app.js
@@ -0,0 +1,7 @@
+ var btnTranslate = document.querySelector("btn-translate");
+ var txtInput = document.querySelector("txt-Input");
+ var txtOutput = document.querySelector("txt-output")
+ 
+ var serverURL = "https://api.funtranslations.com/translate/pig-latin.json";
+
+ 
